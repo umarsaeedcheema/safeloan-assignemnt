edc330336ab35b1a5cdd70fe446f937365996545
Umar Saeed Cheema <23100206@lums.edu.pk>
2024-11-01 23:15:48 +0300

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,4 @@
-// app/layout.tsx
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
-export const metadata: Metadata = {
-  title: "Student Teacher Booking App",
-  description: "Safeloan assignment",
-};
-
+// src/app/layout.tsx
 import "./globals.css";
 
 export default function RootLayout({
